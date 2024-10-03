e7b0aa0239c1356ea3b4ae0cd283f8049dd27097
Alsu Iarulina <al.yarulina@gmail.com>
2024-10-03 15:56:52 +0300

    add new logic

---
+++ dags/12_pipeline_links_new_logic.py
@@ -0,0 +1,30 @@
+import sys
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+
+ 
+default_args = {
+    "owner": "anyarulina",
+    "depends_on_past": False,
+    "start_date": datetime(2024, 6, 24),
+    "email": ["al.yarulina@gmail.com"],
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5),
+}
+
+
+with DAG(
+    dag_id='12_pipeline_links_new_logic',
+    schedule_interval='56 9 17 * *', # каждое 17-ое число
+    catchup=False,
+    default_args=default_args
+    # tags=['yandex'],   
+) as dag:
+    pipeline_links_sberbank = BashOperator(
+    task_id = 'pipeline_links_new_logic',
+    bash_command='python3 /opt/airflow/scripts/yandex_info_reviews_parser/get_links_new_logic.py -path_type 1'
+)
--- scripts/yandex_info_reviews_parser/get_links_new_logic.py
+++ scripts/yandex_info_reviews_parser/get_links_new_logic.py
@@ -13,28 +13,8 @@ from selenium.webdriver.common.by import By
 
 from log import setup_logging
 
-def find_between(s, first, last):
-    """находит строку между символами first и last"""
-    try:
-        regex = rf'{first}(.*?){last}'
-        return re.findall(regex, s)
-    except ValueError:
-        return -1
 
-
-def get_id_from_page(page, bank_name):
-
-    """вытаскивает id банка яндекс карта из page"""
-    first = f'search-snippet-view__link-overlay _focusable" href="/maps/org/{bank_name}/'
-
-    last = '/" tabindex="'
-    yndx_idx = find_between(page, first, last)
-    logging.info(f'loaded {len(yndx_idx)} ids')
-
-    return yndx_idx
-
-
-def get_links_from_city_code(city_code):
+def get_links_from_city_code(city_name):
 
     """формирует список ссыллок-банков для текущего банка id из яндекс карт из раздела Филиалы"""
     
@@ -46,12 +26,13 @@ def get_links_from_city_code(city_code):
     opts.add_argument('headless')
     opts.add_argument('--disable-gpu')
     driver = undetected_chromedriver.Chrome(options=opts)
-    
-    # TODO: где брать коды и латиницы городов ?
-    city_code = "213/moscow/"
-    city_name = city_code.split('/')[1]
 
-    url = f"https://yandex.ru/maps/{city_code}"
+    with open(f'cities_code_dict.pickle', 'rb') as handle:
+            cities_code_dict = pickle.load(handle)
+
+    city_code = cities_code_dict[city_name]
+
+    url = f"https://yandex.ru/maps/{city_code}/category/bank/"
     driver.get(url)
 
     N = round(random.uniform(13.1, 19.9), 2) # was higher
@@ -60,12 +41,16 @@ def get_links_from_city_code(city_code):
     
     t = 'search-snippet-view'
     elements = driver.find_elements(By.CLASS_NAME, t)
-    print(f'LEN {len(elements)}')
+    # print(f'LEN {len(elements)}')
+    print(f'LEN elements at start {len(elements)}')
+
+    links = set()
     
-    if len(elements) > 1:
+    if len(elements) >= 1:
 
         # без этого не работает
         elements = driver.find_elements(By.CLASS_NAME, t)
+        print(f'LEN elements after {len(elements)}')
     
         seen = []
     
@@ -76,6 +61,7 @@ def get_links_from_city_code(city_code):
             N = round(random.uniform(13.1, 19.9), 2) # was higher
             logging.info(f'sleep for {N}')
             time.sleep(N)
+
             elements = driver.find_elements(By.CLASS_NAME, t)
             
             if len(elements) < 1:
@@ -85,30 +71,36 @@ def get_links_from_city_code(city_code):
     
             for i, e in enumerate(elements):
                 print(f'* {i} *')
-                link = e.find_element(By.XPATH, ".//a[@class='search-snippet-view__link-overlay _focusable']").get_attribute('href')
-                print('link', link)
-                links.add(link)
+                link = e.find_element(By.XPATH, ".//a[@class='link-overlay']").get_attribute('href')
+                # link = e.find_element(By.XPATH, ".//a[@class='search-snippet-view__link-overlay _focusable']").get_attribute('href')
+                if 'skameyka' not in link:
+                    print('link', link)
+                    links.add(link)
 
             last_len = len(elements)
             print('LAST LEN ', last_len)
             seen.append(last_len)
             # print('SEEN ', seen)
 
-            with open(f'{path}/links/links_{city_name}.pickle', 'wb') as handle:
+            with open(f'{path}/new_links/links_{city_name}.pickle', 'wb') as handle:
                 pickle.dump(links, handle)
 
             if len(seen) > 2 and seen[-2] == last_len:
                 logging.info(f"{len(links)} links saved for {city_name} city")
                 break
-            
-    
-    else:
-        logging.info('driver stopped')
+
         driver.close()
-        driver.quit()
+        driver.quit()        
+    else:
+        logging.info('LEN IS ZERO')
+        driver.close()
+        driver.quit() 
+    # else:
+    #     logging.info('driver stopped')
+    #     driver.close()
+    #     driver.quit()
         
-    driver.close()
-    driver.quit() 
+    
     
     # return yndx_idx
 
@@ -135,19 +127,9 @@ def get_links_for_cities(cities, path):
     
     for city_name in cities:
 
-        cities_code_dict = {'Москва и область' : '1/moscow-and-moscow-oblast',
-                            'Санкт-Петербург' : '2/saint-petersburg',
-                            'Белгород' : '4/belgorod',
-                            'Калуга' : '6/kaluga', 
-                            'Самара' : '51/samara',
-                            'Воронеж':'193/voronezh', 
-                            'Ульяновск' : '195/ulyanovsk',
+        logging.info(f'I will get links for {city_name} city')
 
-                            "Москва":"213/moscow", 
-                            "Абакан" : "1095/abakan",
-                            "Воркута" : "10940/vorkuta"}
-
-        city_code = cities_code_dict[city_name]
+        
     
         N = round(random.uniform(20.1, 40.9), 2)
         logging.info(f'sleeping for {N} seconds')
@@ -155,7 +137,7 @@ def get_links_for_cities(cities, path):
         
         logging.info(f'get links for city: {city_name}')
         try:
-            get_links_from_city_code(city_code)
+            get_links_from_city_code(city_name)
     
         except Exception as e:
             logging.info(f'Error in get links for city: {city_name}, error: {e}')
@@ -180,4 +162,15 @@ if __name__ == "__main__":
     path = f'{homyak}/parser/scripts/yandex_info_reviews_parser/' if args.path_type==0 else '/opt/airflow/scripts/yandex_info_reviews_parser/'
     setup_logging(path)
     cities = ['Воронеж']
+    cities = ['Санкт-Петербург' ,
+                            'Белгород',
+                            'Калуга' ,
+                            'Самара',
+                            'Ульяновск',
+                            "Москва", 
+                            "Абакан",
+                            "Воркута",
+                            'Владивосток'
+]
+    # TODO: читать список городов из файла 
     get_links_for_cities(cities, path)
